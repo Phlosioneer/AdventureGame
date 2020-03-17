87129aa0e84e690c95449a087889a48b418ed414
Phlosioneer <mattmdrr2@gmail.com>
2020-03-17 01:32:31 -0400

    Replace node-static with express

--- main.js
+++ main.js
@@ -2,54 +2,58 @@ console.log("Starting server");
 
 var fs = require('fs');
 var http = require('http');
-var static = require('node-static');
+var express = require('express');
+var path = require('path');
 
-var staticServer = new static.Server('.', { cache: 0 });
-var defaultPage = "./static/game.html";
+var expressApp = express();
+
+expressApp.use(function(request, response, next) {
+    console.log(request.url);
+    next();
+})
+
+expressApp.get("/", function(request, response, next) {
+    response.redirect("/static/game.html");
+});
+
+expressApp.get("/static/mustache.js", function(request, response, next) {
+    response.sendFile(path.join(__dirname, "node_modules", "mustache", "mustache.js"), error => {
+        if (error) {
+            console.error(error);
+        }
+    });
+});
+
+expressApp.use("/static", express.static(path.join(__dirname, "static")));
+
+expressApp.use("/events/batch", express.static(path.join(__dirname, "events")));
+
+expressApp.get("/events/:eventName.json", function(request, response, next) {
+    var eventName = request.params["eventName"];
+    var fileName = eventLocations[eventName];
+    if (fileName) {
+        response.redirect(fileName);
+    } else {
+        console.log("Could not find any event named: '" + eventName + "'");
+        console.log("Known events:");
+        console.log(eventLocations);
+        response.statusCode = 404;
+        response.end();
+    }
+});
+
+expressApp.use(function(request, response, next, error) {
+    console.error(error);
+});
 
 var eventLocations = {};
 var files = fs.readdirSync("./events/");
 files.forEach(function (file) {
-    var filePath = "./events/" + file;
+    var filePath = path.join(__dirname, "events", file);
     var content = JSON.parse(fs.readFileSync(filePath));
     content.forEach(function(event) {
-        eventLocations[event.name] = filePath;
+        eventLocations[event.name] = "/events/batch/" + file;
     });
 });
 
-http.createServer(function (request, response) {
-    console.log("Received start request");
-    body = [];
-    request.addListener("data", function (chunk) {
-        body.push(chunk);
-    });
-    request.addListener("end", function () {
-        console.log("Received request body. Responding...");
-        console.log(request.url);
-        if (request.url == "/") {
-            staticServer.serveFile(defaultPage, 200, {}, request, response);
-        } else if (request.url == "/static/mustache.js") {
-            staticServer.serveFile("./node_modules/mustache/mustache.js", 200, {}, request, response);
-        } else if (request.url.startsWith("/static/")) {
-            staticServer.serve(request, response);
-        } else if (request.url.startsWith("/events/") && request.url.endsWith(".json")) {
-            var eventName = request.url.substring("/events/".length);
-            eventName = eventName.substring(0, eventName.length - ".json".length);
-            var fileName = eventLocations[eventName];
-            if (fileName) {
-                staticServer.serveFile(fileName, 200, {}, request, response);
-            } else {
-                console.log("Could not find any event named: '" + eventName + "'");
-                console.log("Known events:");
-                console.log(eventLocations);
-                response.statusCode = 404;
-                response.end();
-            }
-        } else {
-            // TODO: Nothing is dynamic yet.
-            console.log("404 not found: " + request.url)
-            response.statusCode = 404;
-            response.end();
-        }
-    });
-}).listen(8080);
+expressApp.listen(8080);
